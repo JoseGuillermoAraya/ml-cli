2ccb7e0bec0e7cf242fbbcec047fe484fe6ba2c1
JoseGuillermoAraya <jose.araya@bci.cl>
2023-02-16 15:53:01 -0300

    Add cross validation

--- src/models/model.py
+++ src/models/model.py
@@ -1,5 +1,6 @@
 import xgboost as xgb
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
 
 class TitanicXGBModel:
     
@@ -25,7 +26,11 @@ class TitanicXGBModel:
         y_pred = self.predict(X_test)
         accuracy = accuracy_score(y_test, y_pred.round())
         print(f"Accuracy: {accuracy:.2f}")
-        
+    
+    def cross_validate(self, X, y, cv=5, scoring='accuracy'):
+        scores = cross_val_score(self.model, X, y, cv=cv, scoring=scoring)
+        return scores.mean()
+
     def get_feature_importance(self):
         if self.model is not None and self.feature_names is not None:
             importance = self.model.get_score(importance_type='gain')
