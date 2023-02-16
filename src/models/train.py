89ade6cd62a063ded3364514acf4cdfd5cb2fa79
JoseGuillermoAraya <jose.araya@bci.cl>
2023-02-16 16:45:01 -0300

    Update train.py

--- src/models/train.py
+++ src/models/train.py
@@ -1,9 +1,21 @@
 import joblib
+import pandas as pd
 from src.models.model import TitanicXGBModel
+from src.data.make_dataset import make_dataset
+from src.data.preprocess_data import preprocess_data
 
-def train_model(X_train, y_train, model_path, model_params):
+def train_model(data_path, model_path, model_params, target):
     # Instantiate the model with the provided parameters
     model = TitanicXGBModel(model_params)
+
+    # Load the training data
+    data = pd.read_csv(data_path)
+
+    # Make data set
+    X_train, y_train = make_dataset(data)
+
+    # Preprocess the data
+    X_train = preprocess_data(X_train)
 
     # Train the model
     model.fit(X_train, y_train)
