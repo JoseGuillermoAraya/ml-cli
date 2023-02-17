13bb1debad2f01248bf815b6983eafe31618cbd0
JoseGuillermoAraya <jose.araya@bci.cl>
2023-02-17 13:44:13 -0300

    Update predict.py

---
+++ ml_cli/ml_cli/commands/predict.py
@@ -0,0 +1,41 @@
+import click
+import joblib
+import pandas as pd
+from data.preprocess_data import preprocess_data
+from utils.logger  import MyLogger
+
+# Define the command-line interface using Click
+@click.command()
+@click.argument('input_file', type=click.Path(exists=True))
+@click.argument('output_file', type=click.Path())
+@click.option('--model-file', default='model.bin', help='Path to the trained model')
+@click.option('--log-file', default='predict.log', help='Path to the log file')
+def predict(input_file, output_file, model_file, log_file):
+    # create a logger object and set up the logging configuration
+    logger = MyLogger(log_file)
+
+    # Load the XGBoost model
+    logger.info(f'Loading model from {model_file}...')
+    model = joblib.load(model_file)
+
+    # Load the input data
+    logger.info(f'Loading data from {input_file}...')
+    X = pd.read_csv(input_file)
+
+    # Preprocess the data
+    logger.info('Preprocessing data...')
+    X = preprocess_data(X)
+
+    # Make predictions using the XGBoost model
+    logger.info('Making predictions...')
+    y_pred = model.predict(X)
+
+    # Save the predictions to a CSV file
+    logger.info(f'Saving predictions to {output_file}...')
+    output_data = pd.DataFrame({'predictions': y_pred})
+    output_data.to_csv(output_file, index=False)
+
+    click.echo(f'Successfully saved predictions to {output_file}')
+
+if __name__ == '__main__':
+    predict()
