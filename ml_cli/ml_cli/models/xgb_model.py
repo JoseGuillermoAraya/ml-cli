3f1d72a342632d8d2889eb9ea027839c309a4cfd
JoseGuillermoAraya <jose.araya@bci.cl>
2023-02-18 17:18:45 -0300

    Add logger

--- ml_cli/ml_cli/models/xgb_model.py
+++ ml_cli/ml_cli/models/xgb_model.py
@@ -1,9 +1,9 @@
 import joblib
 import pandas as pd
 import xgboost as xgb
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
+from ml_cli.utils.logger import get_logger
 
+logger = get_logger(__name__)
 class TitanicXGBModel:
     
     def __init__(self, params):
@@ -14,23 +14,29 @@ class TitanicXGBModel:
         self.feature_importance = None
         
     def fit(self, X_train, y_train):
+        logger.debug("Fitting XGBoost model")
         dtrain = xgb.DMatrix(X_train, label=y_train)
         self.target_name = y_train.name
         self.feature_names = X_train.columns
         self.model = xgb.train(self.params, dtrain)
     
     def save(self, filename):
+        logger.debug(f"Saving XGBoost model to {filename}")
         joblib.dump(self, filename)
 
     def predict(self, X_test):
+        logger.debug("Predicting with XGBoost model")
         if self.model is None:
+            logger.error("XGBoost model is not trained yet")
             raise ValueError("XGBoost model is not trained yet")
         dtest = xgb.DMatrix(X_test)
         y_pred = self.model.predict(dtest)
         return pd.Series(y_pred, name=self.target_name)
 
     def get_feature_importance(self):
+        logger.debug("Getting feature importance from XGBoost model")
         if self.model is None or self.feature_names is None:
+            logger.error("XGBoost model is not trained yet or feature names are not available")
             raise ValueError("XGBoost model is not trained yet or feature names are not available")
         importance = self.model.get_score(importance_type='gain')
         importance = pd.Series(importance, name='importance')
