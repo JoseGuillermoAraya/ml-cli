4e4e7e14b2cdcc55909687986872ee59800641d3
JoseGuillermoAraya <jose.araya@bci.cl>
2023-02-18 19:36:41 -0300

    Update evaluate.py

--- ml_cli/ml_cli/commands/evaluate.py
+++ ml_cli/ml_cli/commands/evaluate.py
@@ -1,44 +1,42 @@
 import click
-import joblib
 import pandas as pd
+import joblib
 from ml_cli.utils.logger import get_logger
+from ml_cli.utils.metrics import evaluate
 from ml_cli.data.make_dataset import make_dataset
 from ml_cli.data.preprocess_data import preprocess_data
 
-# Define the command-line arguments
 @click.command()
-@click.option('--test-file', type=click.Path(exists=True), required=True, help='Path to the test CSV file')
-@click.option('--output-file', type=click.Path(), default='results.csv', help='Path to the output CSV file')
-@click.option('--model-file', default='model.bin', help='Path to the trained model')
-@click.option('--log-file', default='predict.log', help='Path to the log file')
-
-def evaluate(test_file, output_file, model_file, log_file):
+@click.option('--input-file', type=click.Path(exists=True), required=True, help='Path to the input CSV file')
+@click.option('--model-file', type=click.Path(exists=True), required=True, help='Path to the trained model')
+@click.option('--output-file', type=click.Path(), default='evaluation_results.csv', help='Path to the output CSV file')
+@click.option('--log-file', default='evaluate.log', help='Path to the log file')
+def evaluate(input_file, model_file, output_file, log_file):
     logger = get_logger(__name__, log_file)
-    # Load the XGBoost model and feature encoder
+
+    # Load the trained model
     logger.info(f'Loading model from {model_file}...')
     model = joblib.load(model_file)
 
-    # Load the test data into a Pandas DataFrame
-    logger.info(f'Loading data from {test_file}...')
-    X_test, y_test = make_dataset(test_file, 'Survived')
+    # Load the input data
+    logger.info(f'Loading data from {input_file}...')
+    X, y_true = make_dataset(input_file, 'Survived')
 
-    #Preprocess the data
+    # Preprocess the input data
     logger.info('Preprocessing data...')
-    X_test = preprocess_data(X_test)
+    X = preprocess_data(X)
 
-    # Evaluate model
-    logger.info('Evaluating model...')
-    accuracy = model.evaluate(X_test, y_test)
-    cross_validation = model.cross_validate(X_test, y_test)
+    # Make predictions and evaluate the model
+    logger.info('Making predictions and evaluating model...')
+    y_pred = model.predict(X)
+    results = evaluate(y_true, y_pred)
 
-
-    # Save the performance metrics to a CSV file
+    # Save the evaluation results to a CSV file
     logger.info(f'Saving evaluation results to {output_file}...')
-    results = pd.DataFrame({'accuracy': [accuracy], 'cross_validation_score': [cross_validation]})
-    results.to_csv(output_file, index=False)
+    results_df = pd.DataFrame([results])
+    results_df.to_csv(output_file, index=False)
 
-    # Log success message
-    logger.info(f'Successfully saved evaluation results to {output_file}')
+    click.echo(f'Successfully saved evaluation results to {output_file}.')
 
 if __name__ == '__main__':
     evaluate()
