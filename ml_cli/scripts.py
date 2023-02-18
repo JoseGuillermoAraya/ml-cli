844feb96ff14761bad79a9055b2c4c2905135706
JoseGuillermoAraya <jose.araya@bci.cl>
2023-02-18 17:02:51 -0300

    Update test script

--- ml_cli/scripts.py
+++ ml_cli/scripts.py
@@ -5,5 +5,5 @@ def test():
     Run all tests
     """
     subprocess.run(
-        ['pytest', '--cov=ml_cli', '--cov-report', 'html:coverage-report', 'tests/']
+        ['pytest', '--cov=ml_cli', '--cov-report', 'term-missing', '--cov-report', 'html:coverage-report', 'tests/']
     )
