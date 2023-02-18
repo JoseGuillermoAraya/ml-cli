8b0f6c53da9e1532c8849de83cbc2172f547ca95
JoseGuillermoAraya <jose.araya@bci.cl>
2023-02-18 17:11:48 -0300

    Update logger

--- ml_cli/ml_cli/utils/logger.py
+++ ml_cli/ml_cli/utils/logger.py
@@ -1,34 +1,21 @@
 import logging
+import sys
 
-class MyLogger:
-    """
-    A simple logging utility that writes messages to a file and console.
-    """
-    def __init__(self, log_file):
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.DEBUG)
+def get_logger(name, log_file=None):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
 
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    if log_file:
         file_handler = logging.FileHandler(log_file)
         file_handler.setLevel(logging.DEBUG)
-
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(formatter)
-        console_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
-        self.logger.addHandler(file_handler)
-        self.logger.addHandler(console_handler)
-
-    def info(self, msg):
-        self.logger.info(msg)
-
-    def debug(self, msg):
-        self.logger.debug(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def warn(self, msg):
-        self.logger.warn(msg)
+    return logger
